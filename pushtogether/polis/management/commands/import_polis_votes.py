cfbfa5bfeb942fba9b151e79b1b35d2a4f292c23
Marco Silva <marcowilliamdf@gmail.com>
2017-12-20 12:22:04 -0300

    [Votes] Fix import script to report on a better way

--- pushtogether/polis/management/commands/import_polis_votes.py
+++ pushtogether/polis/management/commands/import_polis_votes.py
@@ -24,6 +24,12 @@ class Command(BaseCommand):
         with open(csv_file_votes_path, 'r') as csv_file_votes:
             readf = csv.DictReader(csv_file_votes)
             count = 0
+            votes_from_polis = []
+            conversations_not_found = []
+            comments_not_found = []
+            votes_created = []
+            votes_already_on_db = []
+            print("A importação pode demorar alguns minutos. Importando...")
             for row in readf:
                 xid = row.get('xid')
                 comment_id = row.get('comment_id')
@@ -37,30 +43,34 @@ class Command(BaseCommand):
                 try:
                     conversation = Conversation.objects.get(polis_slug=conversation_slug)
                 except Conversation.DoesNotExist:
-                    self.stdout.write('conversation does not exist, polis_slug: ' + conversation_slug)
+                    conversations_not_found.append(conversation_slug)
                     continue
 
                 try:
                     comment = Comment.objects.get(polis_id=comment_id,
                                                   conversation=conversation.id)
                 except Comment.DoesNotExist:
-                    self.stdout.write('comment does not exist, polis_id: ' + comment_id)
+                    comments_not_found.append(comment_id)
                     continue
 
                 try:
-                    vote = Vote.objects.get(author=user, comment=comment,
-                                            conversation_id=conversation.id)
+                    vote = Vote.objects.get(author=user, comment=comment)
                 except Vote.DoesNotExist:
                     with transaction.atomic():
                         vote = Vote.objects.create(comment=comment, value=vote,
                                                    author=user,
                                                    created_at=created)
-                    print('created vote, polis_id:' + str(vote.polis_id))
+                    votes_from_polis.append(vote.id)
+                    votes_created.append(vote.id)
                     count += 1
                 else:
-                    print('Vote already on database. ID:' + vote.id)
+                    votes_from_polis.append(vote.id)
+                    votes_already_on_db.append(vote.id)
 
-            self.stdout.write('Votes created: ' + str(count))
+            self.stdout.write('Total de votos criados: ' + str(count))
+            self.print_final_report(conversations_not_found, votes_created,
+                                    comments_not_found, votes_already_on_db)
+            self.print_divergences_db_polis(votes_from_polis)
 
     def find_user_by_xid(self, xid):
         if xid:
@@ -81,3 +91,19 @@ class Command(BaseCommand):
             '-1': 1,
         }
         return switcher.get(vote)
+
+    def print_divergences_db_polis(self, votes_from_polis):
+        divergent_votes = Vote.objects.exclude(id__in=set(votes_from_polis))
+        self.stdout.write('Votes on django db but not on polis\':')
+        print(list(divergent_votes.values_list('id', flat=True)))
+
+    def print_final_report(self, conversations_not_found, votes_created,
+                           comments_not_found, votes_already_on_db):
+        self.stdout.write('Conversas não encontradas:')
+        print(set(conversations_not_found))
+        self.stdout.write('Comentários não encontradas:')
+        print(set(comments_not_found))
+        self.stdout.write('Votos criados:')
+        print(set(votes_created))
+        self.stdout.write('Votos que já estavam no banco:')
+        print(set(votes_already_on_db))
